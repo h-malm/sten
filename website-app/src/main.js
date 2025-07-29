8651459aedcc634824f5a5a76b6bf46c89b46bdd
hmalm <hildis.malm@hotmail.com>
2025-07-29 14:52:03 +0300

    Loading screen

--- website-app/src/main.js
+++ website-app/src/main.js
@@ -9,11 +9,9 @@ import './index.css'
 
 const app = createApp(App)
 
-setTimeout(() => {
-  //if you
-  app.mount('#app')
-}, 4000)
-
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue, {})
+setTimeout(() => {
+  app.mount('#app')
+}, 3000)
