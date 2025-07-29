660043193fb3c7a9082156da4e1712c7f32074f7
hmalm <hildis.malm@hotmail.com>
2025-07-29 20:41:48 +0300

    Lazy loading installed

--- website-app/src/main.js
+++ website-app/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import lazyLoad from 'vue-lazyload'
 import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
@@ -11,7 +12,9 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(lazyLoad)
 app.use(PrimeVue, {})
 setTimeout(() => {
   app.mount('#app')
 }, 3000)
+// app.mount('#app')
