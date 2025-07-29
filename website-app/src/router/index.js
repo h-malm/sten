c1a15cf5a4877c0c6a3dbd106fef57f4403b52e0
hmalm <hildis.malm@hotmail.com>
2025-07-29 15:21:08 +0300

    Lazy loading dynamic imports

--- website-app/src/router/index.js
+++ website-app/src/router/index.js
@@ -11,23 +11,21 @@ import KoreanView from '../views/KoreanView.vue'
 import ArtView from '@/views/ArtView.vue'
 import KnittingsView from '@/views/KnittingsView.vue'
 
-const routes = [
-  { path: '/', name: 'home', component: HomeView },
-  { path: '/pageview', name: 'Page', component: PageView },
-  { path: '/about', name: 'About', component: AboutView },
-  { path: '/horror', name: 'Horror', component: HorrorView },
-  { path: '/photos', name: 'Photos', component: PhotosView },
-  { path: '/plants', name: 'Plants', component: PlantView },
-  { path: '/recipes', name: 'Recipes', component: RecipesView },
-  { path: '/crafts', name: 'Crafts', component: CraftsView },
-  { path: '/korean', name: 'Korean Practice', component: KoreanView },
-  { path: '/art', name: 'Art', component: ArtView },
-  { path: '/knitting', name: 'Knitting', component: KnittingsView },
-]
-
 const router = createRouter({
+  routes: [
+    { path: '/', name: 'home', component: HomeView },
+    { path: '/pageview', name: 'Page', component: PageView },
+    { path: '/about', name: 'About', component: AboutView },
+    { path: '/horror', name: 'Horror', component: HorrorView },
+    { path: '/photos', name: 'Photos', component: PhotosView },
+    { path: '/plants', name: 'Plants', component: PlantView },
+    { path: '/recipes', name: 'Recipes', component: RecipesView },
+    { path: '/crafts', name: 'Crafts', component: CraftsView },
+    { path: '/korean', name: 'Korean Practice', component: KoreanView },
+    { path: '/art', name: 'Art', component: ArtView },
+    { path: '/knitting', name: 'Knitting', component: KnittingsView },
+  ],
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
 })
 
 export default router
