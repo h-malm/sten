31b161682a274e541b4a258611b54500321d921f
hmalm <hildis.malm@hotmail.com>
2025-10-31 15:36:22 +0200

    New lighting

--- website-app/src/components/Peaches.vue
+++ website-app/src/components/Peaches.vue
@@ -33,17 +33,11 @@
         const scene = new THREE.Scene();
         scene.background = new THREE.Color( "black" );
 
-        addLight( 0xcc0000, 15, 10, 4, 2 );
-        addLight( 0x7ab920, 10, -12, 2, -5 );
-        addLight( 0xf6b26b, 5, 0, 5, 15 );
+        addDirectionalLight( 0xf6b26b, 10, 5, 1, 10 );
+        addDirectionalLight( 0xf6b26b, 20, -5, 10, -10 );
+        addDirectionalLight( 0xf6b26b, 10, 0, -10, 5 );
 
-        function addAmbientLight ( color ) {
-            const light = new THREE.AmbientLight( color ); // soft white light
-            scene.add( light );
-        }
-
-
-        function addLight ( color, intensity, x, y, z ) {
+        function addDirectionalLight ( color, intensity, x, y, z ) {
             const light = new THREE.DirectionalLight( color, intensity );
             light.position.set( x, y, z );
             light.castShadow = true;
