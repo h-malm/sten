c5204e7ed800beaf6ac62270f1216b21de0bcf72
hmalm <hildis.malm@hotmail.com>
2026-03-12 16:36:10 +0200

    Added yellow sweather knitting project

--- website-app/src/views/KnittingsView.vue
+++ website-app/src/views/KnittingsView.vue
@@ -24,6 +24,7 @@
     import Blueshirt from '../textfiles/knittings/blueshirt.txt?raw'
     import AlpacaShirt from '../textfiles/knittings/alpacka.txt?raw'
     import Gloves from '../textfiles/knittings/gloves.txt?raw'
+    import YellowSweather from '../textfiles/knittings/yellowsweather.txt?raw'
 
     import i1 from '/src/assets/images/knittings/blue.jpg';
     import i2 from '/src/assets/images/knittings/blue1.jpg';
@@ -32,18 +33,22 @@
     import i5 from '/src/assets/images/knittings/alpacka1.jpg';
     import i6 from '/src/assets/images/knittings/alpacka2.jpg';
     import i7 from '/src/assets/images/knittings/alpacka3.jpg';
+
     import i8 from '/src/assets/images/knittings/gloves.jpg';
 
+    import i9 from '/src/assets/images/knittings/gultröja.JPG';
+    import i10 from '/src/assets/images/knittings/gultröja2.JPG';
+    import i11 from '/src/assets/images/knittings/gultröja3.JPG';
+    import i12 from '/src/assets/images/knittings/gultröja4.JPG';
 
     const stories = ref( [
-        { header: 'Blue Knitted Shirt', text: '', image1: '', image2: '', image3: '', image4: '', url: '' },
+        { header: 'Wool Sweather', text: '', image1: '', image2: '', image3: '', image4: '', url: '' },
         { header: 'Alpaca', text: '', image1: '', image2: '', image3: '', image4: '', url: '' },
         { header: 'Gloves', text: '', image1: '', image2: '', image3: '', image4: '', url: '' },
-
+        { header: 'Cotton Sweather', text: '', image1: '', image2: '', image3: '', image4: '', url: '' },
     ] )
 
-    onMounted( () =>
-    {
+    onMounted( () => {
         stories.value[ 0 ].text = Blueshirt;
         stories.value[ 0 ].image1 = i1;
         stories.value[ 0 ].image2 = i2;
@@ -60,16 +65,22 @@
         stories.value[ 2 ].text = Gloves;
         stories.value[ 2 ].image1 = i8;
         stories.value[ 2 ].url = 'https://www.garnstudio.com/pattern.php?id=4222&cid=12';
+
+        stories.value[ 3 ].text = YellowSweather;
+        stories.value[ 3 ].image1 = i9;
+        stories.value[ 3 ].image2 = i10;
+        stories.value[ 3 ].image3 = i11;
+        stories.value[ 3 ].image3 = i12;
+        stories.value[ 3 ].url = 'https://www.garnstudio.com/pattern.php?id=10415&cid=12';
+
     } )
 
-    const formatText = ( text ) =>
-    {
+    const formatText = ( text ) => {
         const paragraphs = text.split( '\n' );
         return paragraphs;
     }
 
-    const getImages = ( item ) =>
-    {
+    const getImages = ( item ) => {
         return [ item.image1, item.image2, item.image3, item.image4 ].filter( image => image && image !== '' );
     }
 </script>
